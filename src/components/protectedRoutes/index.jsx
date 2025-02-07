ef40d0b1bcd4f3ff534675865f28ef3119f8b63e
Akwaji Dan Uke <danakwaji3@gmail.com>
2025-02-08 00:32:17 +0100

    testting fancy spinner

--- src/components/protectedRoutes/index.jsx
+++ src/components/protectedRoutes/index.jsx
@@ -10,7 +10,16 @@ const ProtectedRoutes = () => {
   const location = useLocation();
 
   if (loading) {
-    return <div className="flex justify-center items-center h-screen">...</div>;
+    return (
+      <div className="flex justify-center items-center h-screen">
+        <div class="flex items-center justify-center h-screen">
+          <div class="relative">
+            <div class="h-24 w-24 rounded-full border-t-8 border-b-8 border-gray-200"></div>
+            <div class="absolute top-0 left-0 h-24 w-24 rounded-full border-t-8 border-b-8 border-blue-500 animate-spin"></div>
+          </div>
+        </div>
+      </div>
+    );
   } else {
     return user ? (
       <Outlet />
